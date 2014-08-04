877b03d3050891fdb564df8d64e8540ca7d6c988
LNA <LAllen@8thLight.com>
2014-08-04 17:02:28 -0500

    YASSS

---
+++ micro-blog/spec/micro_blog/core_spec.clj
@@ -0,0 +1,7 @@
+(ns micro-blog.core-spec
+  (:require [speclj.core :refer :all]
+            [micro-blog.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ micro-blog/src/micro_blog/core.clj
@@ -0,0 +1,1 @@
+(ns micro-blog.core)
