921762d0e521ed8e0ffd658d04acf79037649773
LNA <LAllen@8thLight.com>
2014-08-13 15:05:53 -0500

    Creates hash map of senders and content

--- micro-blog/src/microblog/tweet_parser.clj
+++ micro-blog/src/microblog/tweet_parser.clj
@@ -1,14 +1,13 @@
 (ns microblog.tweet-parser)
 (use 'clojure.java.io)
 
+(defn name-of-sender [line]
+  (re-find #"^[^:]+" line))
+
+(defn content [line]
+  (re-find #"[^:]+$" line))
+
 (defn parse-tweets [file]
   (with-open [rdr (reader file)]
     (doseq [line (line-seq rdr)]
-        (println line))))
-
-(defn name-of-sender [line]
-  (println  (re-find #"^[^:]+" line)))
-
-(defn content [line]
-  (println  (re-find #"[^:]+$" line)))
-
+        (println { :sender (name-of-sender line) :content (content line)}))))
