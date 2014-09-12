d012ce5f56edf18a1b3fd281832bab33366c4852
LNA <LAllen@8thLight.com>
2014-09-12 18:33:02 -0500

    Removes describe statements from tests

--- micro-blog/spec/microblog/tweet_parser_spec.clj
+++ micro-blog/spec/microblog/tweet_parser_spec.clj
@@ -2,22 +2,19 @@
   (:require [speclj.core :refer :all]
             [microblog.tweet-parser :refer :all]))
 
-(describe "name-of-sender"
+(describe "tweet-parser"
   (it "returns the name of a sender"
     (should="test_sender" 
-      (name-of-sender "test_sender: foo bar"))))
+      (name-of-sender "test_sender: foo bar")))
 
-(describe "content"
   (it "returns the content"
     (should=" foo bar" 
-      (content "test_sender: foo bar"))))
+      (content "test_sender: foo bar")))
 
-(describe "receivers"
   (it "returns all names of receivers"
     (should= ["foo" "bar"] 
-      (receivers "test_sender: @foo was @bar"))))
+      (receivers "test_sender: @foo was @bar")))
 
-(describe "parse-tweets"
   (it "returns a hash-map of a tweets content and sender"
     (should=
       '({:content " hi @bob!", :sender "roberta" :receivers ["bob"]})
